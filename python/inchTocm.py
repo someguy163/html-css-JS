7ce952192ced15d7a62eb2d1f1d8d3fc4e8adac7
someguy163 <wnguddl163@naver.com>
2024-04-01 12:00:55 +0900

    2024-04-01 py

---
+++ python/inchTocm.py
@@ -0,0 +1,10 @@
+print("숫자입력")
+x = input()
+print(float(x) , "inch")
+print(float(x) * 2.54 , "cm")
+
+str_input = input("숫자입력")
+num_input = float(str_input)
+print()
+print(num_input , "inch")
+print((num_input * 2.54),"cm")
---
+++ python/input.py
@@ -0,0 +1,3 @@
+print("Enter your name")
+somebody = input()
+print("HI " ,somebody , "How are you today")
